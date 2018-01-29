83bbbabbcba5f5dde903e66287bca8d776970f99
chuangchuang.wang <extern.chuangchuang.wang@audi.com.cn>
2018-01-29 15:39:56 +0800

    create a new project

---
+++ SwiftLintDmeo/SwiftLintDmeo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftLintDmeo
+//
+//  Created by chuangchuang wang on 2018/1/29.
+//  Copyright © 2018年 chuangchuang wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
