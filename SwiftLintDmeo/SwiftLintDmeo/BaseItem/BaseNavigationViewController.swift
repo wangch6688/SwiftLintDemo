8c3d952c63dbc3f66130b3fdac5a9bd6d80b5bb1
chuangchuang.wang <extern.chuangchuang.wang@audi.com.cn>
2018-01-30 14:42:23 +0800

    1. add base view controller (viewcontroller and navigationViewcontroller)

---
+++ SwiftLintDmeo/SwiftLintDmeo/BaseItem/BaseNavigationViewController.swift
@@ -0,0 +1,81 @@
+//
+//  BaseNavigationViewController.swift
+//  SwiftLintDmeo
+//
+//  Created by chuangchuang wang on 2018/1/30.
+//  Copyright © 2018年 chuangchuang wang. All rights reserved.
+//
+
+import UIKit
+
+class BaseNavigationController: UINavigationController {
+    
+    enum CustomBarStyle {
+        case `default`
+        case light
+    }
+    
+    var barStyle: CustomBarStyle = .default {
+        didSet {
+            self.setNeedsStatusBarAppearanceUpdate()
+        }
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        if self.barStyle == .default {
+            return .default
+        } else {
+            return .lightContent
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let font = UIFont(name: "normalFontIdentifier", size: 15) {
+            self.navigationBar.titleTextAttributes = [
+                .foregroundColor: UIColor.black,
+                .font: font
+            ]
+        }
+        self.navigationBar.isTranslucent = false
+    }
+    
+    override func pushViewController(_ viewController: UIViewController, animated: Bool) {
+        super.pushViewController(viewController, animated: animated)
+        
+        if self.viewControllers.count > 1 {
+            let backButton = UIButton(type: .custom)
+            let backArrowImage = UIImage(named: "back-meta-small", in: nil, compatibleWith: nil)
+            backButton.setImage(backArrowImage, for: .normal)
+            backButton.frame = CGRect(x: 0, y: 0, width: 24, height: 24)
+            backButton.addTarget(self, action: #selector(tapBack(sender:)), for: .touchUpInside)
+            let leftBarButtonItem = UIBarButtonItem(customView: backButton)
+            viewController.navigationItem.leftBarButtonItem = leftBarButtonItem
+            
+        } else {
+            let nilBackBarButtonItem = UIBarButtonItem(customView: UIView())
+            viewController.navigationItem.leftBarButtonItem = nilBackBarButtonItem
+        }
+    }
+    
+    @objc func tapBack(sender: UIButton) {
+        self.popViewController(animated: true)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
---
+++ SwiftLintDmeo/SwiftLintDmeo/BaseItem/BaseViewController.swift
@@ -0,0 +1,99 @@
+//
+//  BaseViewController.swift
+//  SwiftLintDmeo
+//
+//  Created by chuangchuang wang on 2018/1/30.
+//  Copyright © 2018年 chuangchuang wang. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController, UIGestureRecognizerDelegate {
+    
+    enum CustomBarStyle {
+        case `default`
+        case light
+    }
+    
+    var barStyle: CustomBarStyle = .default {
+        didSet {
+            self.setNeedsStatusBarAppearanceUpdate()
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.navigationBar.layer.shadowColor = UIColor.black.cgColor
+        self.navigationController?.navigationBar.layer.shadowOffset = CGSize(width: 0, height: 1)
+        self.navigationController?.navigationBar.layer.shadowOpacity = 0.2
+        if let rect = self.navigationController?.navigationBar.bounds {
+            self.navigationController?.navigationBar.layer.shadowPath = UIBezierPath(rect: rect).cgPath
+            self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarPosition.any, barMetrics: UIBarMetrics.default)
+            self.navigationController?.navigationBar.shadowImage = UIImage()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 全屏右滑返回手势
+        /*let target = navigationController?.interactivePopGestureRecognizer?.delegate
+         let panGesture = UIPanGestureRecognizer(target: target, action: #selector(handleNavigationTransition(_:)))
+         panGesture.delegate = self
+         view.addGestureRecognizer(panGesture)
+         navigationController?.interactivePopGestureRecognizer?.isEnabled = false*/
+        
+        //系统右滑返回手势
+        navigationController?.interactivePopGestureRecognizer?.delegate = self
+        
+        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.white
+        self.automaticallyAdjustsScrollViewInsets = false
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        if self.barStyle == .default {
+            return .default
+        } else {
+            return .lightContent
+        }
+    }
+    
+    func handleNavigationTransition(_ sender: UIPanGestureRecognizer) {
+        
+    }
+    
+    func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        if navigationController?.childViewControllers.count == 1 {
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
+/*
+ extension BaseViewController: NotificationButtonPressedProtocol {
+ func bottomButtonHasBeenPressed() {}
+ }
+ 
+ extension BaseViewController: NotificationCancelPressedProtocol {
+ func cancelHasbeenPressed() {}
+ }
+ */
+
